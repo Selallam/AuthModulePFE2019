412b378239d8bbb01470ddbde8694782cf51dd38
saad <saadc06@gmail.com>
2019-04-11 13:31:40 +0100

    debut

---
+++ AuthenticationModule/src/main/java/org/app/auth/security/repository/AppUserRepository.java
@@ -0,0 +1,8 @@
+package org.app.auth.security.repository;
+
+import org.app.auth.model.security.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+	AppUser findByUsername(String username);
+}
